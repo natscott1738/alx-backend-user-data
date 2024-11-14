e6a67b4b6955e9b0f9c1cf5cd64436ef3999904a
natscott1738 <natebaraka777@gmail.com>
2024-11-14 20:51:50 +0300

    session auth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Session Authentication Module
+"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
+
+
+class SessionAuth(Auth):
+    """Session Authentication Class"""
+    pass
