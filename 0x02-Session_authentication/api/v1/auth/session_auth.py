7056f245d282d7400a6eff7e0cd5a3a03e3335d1
natscott1738 <natebaraka777@gmail.com>
2024-11-14 22:53:39 +0300

    update session auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -42,4 +42,25 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
 
         return User.get(user_id)
-    pass
+
+    def destroy_session(self, request=None):
+        """Deletes user session/ logout"""
+
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        try:
+            del self.user_id_by_session_id[session_id]
+        except Exception:
+            pass
+
+        return True
